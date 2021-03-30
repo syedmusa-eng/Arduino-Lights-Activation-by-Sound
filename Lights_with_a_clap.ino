c9fe362b6f5d8bac6d8ab4cafb8e93f882649487
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-03-30 15:15:40 +0200

    Final Version that I tested

---
+++ Lights_with_a_clap.ino
@@ -0,0 +1,54 @@
+// www.elegoo.com
+// 2018.10.24
+/*
+  LED1 should be lit, showing power. LED2 indicates sound input, and the sensitivity is adjusted by potentiometer.
+  There is a tiny screw on the blue potentiometer block that you can use for adjustment. Turning that
+  clockwise lowers the potentiometer value, while counter-clockwise raises the potentiometer value.
+  Use the potentiometer to adjust the Sound Sensor sensitivity. Turn the potentiometer
+  several rotations until you see the LED2 extinguish (or just faintly blink). This might be slightly greater than
+  500, if you are also watching Serial Monitor (inital adjustment), or, Serial Plotter (the latter is prefererd for observation).
+  Special thanks to user CRomer, for his input and hard work!
+*/
+
+
+
+int  sensorAnalogPin = A0;    // Select the Arduino input pin to accept the Sound Sensor's analog output 
+int  sensorDigitalPin = 7;    // Select the Arduino input pin to accept the Sound Sensor's digital output
+int  analogValue = 0;         // Define variable to store the analog value coming from the Sound Sensor
+int  digitalValue;            // Define variable to store the digital value coming from the Sound Sensor
+int  Led13 = 13;              // Define LED port; this is the LED built in to the Arduino (labled L)
+                              // When D0 from the Sound Sensor (connnected to pin 7 on the
+                              // Arduino) sends High (voltage present), L will light. In practice, you
+                              // should see LED13 on the Arduino blink when LED2 on the Sensor is 100% lit.
+int Light = 3;                // output for the lights to be on at the recognition of sound.                              
+
+
+void setup()
+{
+  Serial.begin(9600);               // The IDE settings for Serial Monitor/Plotter (preferred) must match this speed
+  pinMode(sensorDigitalPin,INPUT);  // Define pin 7 as an input port, to accept digital input
+  pinMode(Led13,OUTPUT);            // Define LED13 as an output port, to indicate digital trigger reached
+  pinMode(Light,OUTPUT);            // Define pin3 as an output pin, to light up the leds
+}
+
+void loop(){
+  analogValue = analogRead(sensorAnalogPin); // Read the value of the analog interface A0 assigned to digitalValue 
+  digitalValue=digitalRead(sensorDigitalPin); // Read the value of the digital interface 7 assigned to digitalValue 
+  Serial.println(analogValue); // Send the analog value to the serial transmit interface
+  Serial.println(digitalValue); // Send the digital value to the serial transmit interface
+  
+  if(digitalValue==HIGH)      // When the Sound Sensor sends signal, via voltage present, light LED13 (L)
+  {
+    digitalWrite(Led13,HIGH);
+    digitalWrite(Light,HIGH); 
+    delay(5000);
+    digitalWrite(Light,LOW);
+  }
+  /*else
+  {
+    digitalWrite(Led13,LOW);
+    digitalWrite(Light,LOW);
+  }*/
+  
+  delay(500);                  // Slight pause so that we don't overwhelm the serial interface
+}
